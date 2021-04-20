e5dc4bc6572c10e5ff648eb7281a1d775035756c
RafaJF <61872929+RafaJF@users.noreply.github.com>
2021-04-20 18:19:46 +0200

    CauseValidator

---
+++ src/main/java/org/springframework/samples/petclinic/util/CauseValidator.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.util;
+
+import org.springframework.samples.petclinic.model.Cause;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class CauseValidator implements Validator{
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return Cause.class.isAssignableFrom(clazz);
+	}
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		Cause cause= (Cause) target;
+
+		Double goal = cause.getGoal();
+		
+		
+		if (goal <= 0) {
+			errors.rejectValue("goal", "El valor del objetivo debe ser mayor que 0",
+					"El valor del objetivo debe ser mayor que 0");
+		}
+		
+		if(goal > 20000) {
+			errors.rejectValue("goal", "El objetivo marcado no puede ser mayor a 20000",
+					"El objetivo marcado no puede ser mayor a 20000");
+		}
+		
+	
+
+	}
+
+}
